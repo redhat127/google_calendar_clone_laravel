bda267379f495e95cef037ab6ea73aeec8d4fdc5
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-06 08:49:44 +0330

    test: update feature test and use lazy database refresh

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,9 @@
 <?php
 
-test('returns a successful response', function () {
-    $response = $this->get('/');
+use function Pest\Laravel\get;
+
+test('home returns 200 response', function () {
+    $response = get('/');
 
     $response->assertStatus(200);
 });
